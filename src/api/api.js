e1484047fe77f902be1dc503bbe52eb0158004e6
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-11-13 00:00:04 +0300

    add a function to update profile image styled drop menu

--- src/api/api.js
+++ src/api/api.js
@@ -2,29 +2,40 @@ import axios from "axios"
 
 
 const instance = axios.create({
-	baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-	withCredentials: true,
-	headers: {
-		"API-KEY": "40bb3342-9f88-4621-be93-98a913ff7653",
-	}
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        "API-KEY": "40bb3342-9f88-4621-be93-98a913ff7653",
+    }
 })
 
 export const usersAPI = {
-	getUsers: (page = 1, pageSize = 5) => instance.get(`users?page=${page}&count=${pageSize}`).then(responce => responce.data),
-	follow: (userId) => instance.post(`follow/${userId}`, {}).then(responce => responce.data),
-	unFollow: (userId) => instance.delete(`follow/${userId}`, {}).then(responce => responce.data)
+    getUsers: (page = 1, pageSize = 5) => instance.get(`users?page=${page}&count=${pageSize}`).then(responce => responce.data),
+    follow: (userId) => instance.post(`follow/${userId}`, {}).then(responce => responce.data),
+    unFollow: (userId) => instance.delete(`follow/${userId}`, {}).then(responce => responce.data)
 }
 
 export const profileAPI = {
-	getProfile: (userId) => instance.get(`profile/${userId}`).then(responce => responce.data),
-	getStatus: (userId) => instance.get(`profile/status/${userId}`).then(responce => responce.data),
-	updateStatus: (status) => instance.put(`profile/status`, { status }).then(responce => responce.data),
-
+    getProfile: (userId) => instance.get(`profile/${userId}`).then(responce => responce.data),
+    getStatus: (userId) => instance.get(`profile/status/${userId}`).then(responce => responce.data),
+    updateStatus: (status) => instance.put(`profile/status`, {status}).then(responce => responce.data),
+    savePhoto: (file) => {
+        const formData = new FormData()
+        formData.append('image', file)
+        return instance.put(`profile/photo`, formData, {
+            headers: "Content-Type: multipart/form-data"
+        })
+            .then(responce => responce.data)
+    },
 }
 
 export const loginAPI = {
-	auth: () => instance.get(`auth/me`).then(responce => responce.data),
-	userLogin: (email, password, rememberMe = false) => instance.post(`auth/login`, { email, password, rememberMe }).then(responce => responce.data),
-	userLogout: () => instance.delete(`auth/login`).then(responce => responce.data),
+    auth: () => instance.get(`auth/me`).then(responce => responce.data),
+    userLogin: (email, password, rememberMe = false) => instance.post(`auth/login`, {
+        email,
+        password,
+        rememberMe
+    }).then(responce => responce.data),
+    userLogout: () => instance.delete(`auth/login`).then(responce => responce.data),
 
 }
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,88 +1,104 @@
 import postsData from "../data/profileData/Posts/PostsData";
 import users from "../data/profileData/UserData";
 
-import { profileAPI } from "../api/api";
+import {profileAPI} from "../api/api";
+
 const ADD_POST = "ADD-POST"
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT"
 const SET_USER_PROFILE = "SET_USER_PROFILE"
 const SET_USER_STATUS = "SET_USER_STATUS"
+const SAVE_PHOTO_SUCCESS = "SAVE_PHOTO_SUCCESS"
 
 
 const initialState = {
-	defaultUser: users,
-	profile: null,
-	status: "",
-	postsData,
-	values: {
-		postState: ""
-	}
+    defaultUser: users,
+    profile: null,
+    status: "",
+    postsData,
+    values: {
+        postState: ""
+    }
 }
 const profileReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case ADD_POST:
-			const newPost = { //Создание нового поста
-				id: state.postsData.length + 1,
-				post__authorName: "ShaggyBa",
-				post__authorImage: "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1213&q=80",
-				post__text: action.value,
-				post__likes: 0,
-				post__images: ""
-			}
+    switch (action.type) {
+        case ADD_POST:
+            const newPost = { //Создание нового поста
+                id: state.postsData.length + 1,
+                post__authorName: "ShaggyBa",
+                post__authorImage: "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1213&q=80",
+                post__text: action.value,
+                post__likes: 0,
+                post__images: ""
+            }
 
-			return { ...state, postsData: [newPost, ...state.postsData], values: { postState: "" } }
-		case UPDATE_NEW_POST_TEXT:
-			return { ...state, values: { postState: action.value } }
-		case SET_USER_PROFILE:
-			return { ...state, profile: action.profile }
-		case SET_USER_STATUS:
-			return { ...state, status: action.status }
-		default:
-			return { ...state }
-	}
+            return {...state, postsData: [newPost, ...state.postsData], values: {postState: ""}}
+        case UPDATE_NEW_POST_TEXT:
+            return {...state, values: {postState: action.value}}
+        case SET_USER_PROFILE:
+            return {...state, profile: action.profile}
+        case SET_USER_STATUS:
+            return {...state, status: action.status}
+        case SAVE_PHOTO_SUCCESS:
+            return {...state, profile: {...state.profile, photos: action.photos}}
+        default:
+            return {...state}
+    }
 }
 
 export const addPost = (value = "") =>
-({
-	type: ADD_POST,
-	value
-})
+    ({
+        type: ADD_POST,
+        value
+    })
 
 export const updateNewPostText = (value) =>
-({
-	type: UPDATE_NEW_POST_TEXT,
-	value
-})
+    ({
+        type: UPDATE_NEW_POST_TEXT,
+        value
+    })
 
 export const setUserProfile = (profile) => ({
-	type: SET_USER_PROFILE,
-	profile
+    type: SET_USER_PROFILE,
+    profile
 })
 
 export const setUserStatus = (status) => ({
-	type: SET_USER_STATUS,
-	status
+    type: SET_USER_STATUS,
+    status
 })
 
 
 export const getUserProfile = (userId) => async (dispatch) => {
-	if (userId) {
-		const data = await profileAPI.getProfile(userId)
-		dispatch(setUserProfile(data))
-	}
+    if (userId) {
+        const data = await profileAPI.getProfile(userId)
+        dispatch(setUserProfile(data))
+    }
 }
 
 export const getStatus = (userId) => async (dispatch) => {
-	if (userId) {
-		const data = await profileAPI.getStatus(userId)
-		dispatch(setUserStatus(data))
-	}
+    if (userId) {
+        const data = await profileAPI.getStatus(userId)
+        dispatch(setUserStatus(data))
+    }
 }
 
 export const updateStatus = (status) => async (dispatch) => {
-	const data = await profileAPI.updateStatus(status)
-	if (data.resultCode === 0)
-		dispatch(setUserStatus(status))
+    const data = await profileAPI.updateStatus(status)
+    if (data.resultCode === 0)
+        dispatch(setUserStatus(status))
 }
 
+export const savePhotoSuccess = (photos) => ({
+    type: SAVE_PHOTO_SUCCESS,
+    photos
+})
+
+export const savePhoto = (file) => async (dispatch) => {
+    const data = await profileAPI.savePhoto(file)
+    if (data.resultCode === 0) {
+        console.log("photo upload", data.data.photos)
+        dispatch(savePhotoSuccess(data.data.photos))
+    }
+}
 
 export default profileReducer
